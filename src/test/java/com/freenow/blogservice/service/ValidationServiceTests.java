d7ed53668e4325491ccb8131c8f9b64b64ee925f
Jony <jkhatri@qualys.com>
2020-07-12 01:26:31 +0530

    Added some tests for valid and invalid email address.

--- src/test/java/com/freenow/blogservice/service/ValidationServiceTests.java
+++ src/test/java/com/freenow/blogservice/service/ValidationServiceTests.java
@@ -3,14 +3,19 @@ package com.freenow.blogservice.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Validate Email addresses and other validations")
 @ExtendWith(SpringExtension.class)
@@ -20,11 +25,24 @@ public class ValidationServiceTests {
     @Autowired
     private ValidationService validationService;
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("listOfEmails")
     @DisplayName("List of invalid Email addresses")
-    public void shouldFindInvalidEmails(){
-        List<String> emailIds= Arrays.asList("");
+    public void shouldFindInvalidEmails(List<String> emailIds){
         List<String> emails = validationService.findInvalidEmailAddresses(emailIds);
+        assertAll(
+                ()->assertTrue(!emails.isEmpty()),
+                ()->assertEquals(emails.size(), 2),
+                ()->assertEquals(emails.get(0), "#@#@#"),
+                ()->assertEquals(emails.get(1), "abc")
+        );
         assertTrue(!emails.isEmpty());
+
     }
+
+    private static Stream<Arguments> listOfEmails(){
+        return Stream.of(
+                Arguments.of(Arrays.asList("#@#@#","abc","abc@xyz.com","jony@freenow.com")));
+    }
+
 }
